7cc65d317e5fd0466fd62e02192ae3c8dac593fd
EhtashamAli <ehtasham.ali1979@gmail.com>
2019-10-08 11:45:13 +0500

    build

---
+++ build/precache-manifest.ded5918ed58496ca8594e88f1863f61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291caeab2f93b77b79369812263fe11d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c07d71b0919dd5078791",
+    "url": "/static/css/main.e13ebf79.chunk.css"
+  },
+  {
+    "revision": "6f26cdaf5fa093da5a63",
+    "url": "/static/js/2.ba1e478c.chunk.js"
+  },
+  {
+    "revision": "c07d71b0919dd5078791",
+    "url": "/static/js/main.25a0eb1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
